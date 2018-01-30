a166cf29819b67d0838cc95b68ce0af4d2304626
Alyssa Kim <alyssa.kim@gmail.com>
2018-01-30 19:54:35 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,55 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+  [0, 3, 6], 
+  [1, 4, 7],
+  [2, 5, 8]
+  ]
+  
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_combination.each do |winning_array|
+      if (winning_array.include?(board))
+      winning_array
+      else 
+      false 
+      end
+    end
+  end
+end
+
+def full?(board)
+  board.all? do |token|
+    if (token == "X" || token == "O")
+    true 
+    else 
+    false 
+    end
+  end
+end
+
+def draw?(board)
+  if (full?(board) == true && won?(board) == false)
+    true
+  elsif ((full?(board) == false && won?(board) == true) && (won?(board) == false))
+    false
+  end
+end
+
+def over?(board)
+  if (won?(board) == true || draw?(board) == true || full?(board) == true) || (won?(board) == true && full?(board) == false)
+    true
+  elsif (won?(board) == false || draw?(board) == false || full?(board) == false)
+    false
+  end
+end
+
+def winner(board)
+  
+end
