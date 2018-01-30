5871e947796b71c1cb6222fa18cb4d19af537e3b
Alyssa Kim <alyssa.kim@gmail.com>
2018-01-30 20:24:10 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,7 +17,7 @@ WIN_COMBINATIONS = [
   
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-    if win_combination
+    if (win_combo[0] == "X" && win_combo[1] == "X" && win_combo[2] == "X") || (win_combo[0] == "O" && win_combo[1] == "O" && win_combo[2] = "O"))
       winning_array
     else
       false
